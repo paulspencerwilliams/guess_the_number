d8542019cdc993a85c49db78d09c63828fd5a841
Paul Williams <paulswilliams@me.com>
2012-10-18 17:55:29 +0100

    2nd scenario failling..

--- features/guess_number.feature
+++ features/guess_number.feature
@@ -2,3 +2,8 @@ Feature: guess a number
 Scenario: prompt for number
   When I visit homepage
   Then I should be prompted to guess number
+
+ Scenario: guess wrong
+ 	Given I visit homepage
+ 	When I guess 45
+ 	Then I should be advised to guess higher
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -5,3 +5,11 @@ end
 Then /^I should be prompted to guess number$/ do
   page.should have_content('Guess a number')
 end
+
+When /^I guess (\d+)$/ do |my_guess|
+	fill_in 'My Guess', :with => my_guess
+end
+
+Then /^I should be advised to guess higher$/ do
+  pending # express the regexp above with the code you wish you had
+end
