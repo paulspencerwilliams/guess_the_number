d9b14bc1d4661d33b736c6dcbc39e87f11657b47
Paul Williams <paulswilliams@me.com>
2012-10-18 17:16:16 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuessTheNumber::Application.config.secret_token = '00de3de1eb38ad5970ce4023a616ed92497317cea4afac4022dc5b800528d7885a6a74f891c4a3a19650ffecc23aad9dc350facd71cd031bee26ce67deacc4ea'
