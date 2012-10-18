ce7be7638967fdb3e587ec22f1c77c4dff2b282b
Paul Williams <paulswilliams@me.com>
2012-10-18 17:29:38 +0100

    added first feature

---
+++ features/guess_number.feature
@@ -0,0 +1,4 @@
+Feature: guess a number
+Scenario: prompt for number
+  When I visit homepage
+  Then I should be prompted to guess number
---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,7 @@
+When /^I visit homepage$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be prompted to guess number$/ do
+  pending # express the regexp above with the code you wish you had
+end
